c0d106f45161b099b8030942fa96ded93d8ec4d7
g3rry9898 <hydrogenbond36@gmail.com>
2024-11-17 22:12:32 -0800

    l

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """
-This module provides a function to obfuscate log messages.
+This module provides a Redacting Formatter for logging.
 """
 
-import re
+import logging
 from typing import List
+import re
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """
@@ -21,4 +22,28 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
     """
     pattern = f"({'|'.join(fields)})=[^{separator}]*"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Formats the log record by obfuscating specified fields.
+        
+        Args:
+            record (logging.LogRecord): The log record to format.
+        
+        Returns:
+            str: The formatted log record.
+        """
+        return filter_datum(self.fields, self.REDACTION, super().format(record), self.SEPARATOR)
 
